a7160cf29946d6d1409c128492f9cabcbc1a54fd
David Jarvis <ddjarvis@up.edu.ph>
2024-08-02 15:05:17 +0800

    Update properCase.js

--- js/properCase.js
+++ js/properCase.js
@@ -1,6 +1,6 @@
 function properCase(str) {
 	return str.replace(/\b\w+(\'\w+)?\b/g, word => {
-		if (word.length <= 3 && !/[aeiou]/i.test(word)) {
+		if (word.length <= 4 && !/[aeiou]/i.test(word)) {
 			return word.toUpperCase();
 		} else {
 			return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
